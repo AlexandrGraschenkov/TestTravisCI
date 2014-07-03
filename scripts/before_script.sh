fde58ca222a667176c1495c2282f9c3a6ed6b102
Alexander <sharrikk@gmail.com>
2014-07-03 17:00:14 +0400

    - fix before script

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -5,5 +5,5 @@ brew update
 brew upgrade xctool
 xctool --version
 openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/profile/AlexGraAdHoc.mobileprovision.enc -d -a -out scripts/profile/AlexGraAdHoc.mobileprovision
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.cer.enc -d -a -out scripts/certs/dist.cer
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/certs/dist.p12.enc -d -a -out scripts/certs/dist.p12
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/cert/dist.cer.enc -d -a -out scripts/cert/dist.cer
+openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/cert/dist.p12.enc -d -a -out scripts/cert/dist.p12
