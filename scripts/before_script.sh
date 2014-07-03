0d1849854963e846ee197add36f924a2838b0abd
Alexander <sharrikk@gmail.com>
2014-07-03 17:37:27 +0400

    12312312

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -4,6 +4,3 @@ set -e
 brew update
 brew upgrade xctool
 xctool --version
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/profile/AlexGraAdHoc.mobileprovision.enc -d -a -out scripts/profile/AlexGraAdHoc.mobileprovision
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/cert/dist.cer.enc -d -a -out scripts/cert/dist.cer
-openssl aes-256-cbc -k "$ENCRYPTION_SECRET" -in scripts/cert/dist.p12.enc -d -a -out scripts/cert/dist.p12
